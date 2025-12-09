81b96484fdd1a12fb4fdbb14bb28316de6cadf3d
kaithery123 <revathiklucky@gmail.com>
2025-12-09 15:30:59 +0530

    second commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
-print("you have 4 options", end="")
-print("Addition-1")
-print("subtraction-2")
-print("multiplication-3")
+print("you have 4 options")
+print("Addition- 1")
+print("subtraction- 2")
+print("multiplication- s3")
 print("divisionn-4")
 def add(a,b):
     return a+b
@@ -14,10 +14,10 @@ def div(a,b):
 
 def calculator():
     s=True
-    a=int(input('enter the first number'))
+    a=int(input('enter the first number '))
     while s :
-        b = int(input('enter the second number'))  
-        choice = int(input("enter your choice"))
+        b = int(input('enter the second number '))  
+        choice = int(input("enter your choice "))
         if choice == 1:
             a = add(a,b)
             print(a)
@@ -30,10 +30,10 @@ def calculator():
         elif choice == 4:
             if b==0:
                 print("divid by zero not possible")
-                b = int(input('enter the second number'))
+                b = int(input('enter the second number '))
             a = div(a,b)
             print(a)
-        w = input("do you want to Continue / Restart / Stop c/r/s").lower()
+        w = input("do you want to Continue / Restart / Stop c/r/s ").lower()
         if w == 'c':
             continue
         elif w == 'r':
